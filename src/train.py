aff19a221af574ce6fb2c76a68f4a2c36335004a
matlin100 <107589087+matlin100@users.noreply.github.com>
2025-04-20 11:07:53 +0300

    train

--- src/train.py
+++ src/train.py
@@ -1,5 +1,6 @@
-import sys; sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import json
 from typing import List, Dict, Tuple
 import random
